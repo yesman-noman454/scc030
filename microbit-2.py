50f9bd7200ffc16517410b131e1acceda85d8136
yesman-noman454 <454jamieowen@gmail.com>
2022-04-01 20:49:48 +0100

    multiple images added needs more

--- microbit-2.py
+++ microbit-2.py
@@ -1,9 +1,47 @@
 from microbit import *
 import radio
 radio.config(channel=0)
+zero=Image("50505:"
+    "05050:"
+    "50505:"
+    "05050:"
+    "50505")
 
+one=Image("00000:"
+    "05050:"
+    "05550:"
+    "00500:"
+    "00000")
+
+two=Image("00500:"
+    "05050:"
+    "50005:"
+    "05050:"
+    "00500")
+
+three=Image("00000:"
+    "05550:"
+    "05050:"
+    "05550:"
+    "0000")
+
+four=Image()
+
+five=Image()
+
+six=Image()
+
+seven=Image()
+
+eight=Image()
+
+nine=Image()
+
+symbols=[one,two,three,four,five,six,seven,eight,nine]
 def cycle(a):
-
+    for i in range(5):
+        display.show(symbols,delay=100)
+    display.show(symbols[a])
 
 while True:
     transmit=radio.recive()
