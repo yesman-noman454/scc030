22074e709fb33c49eeee8b8487beff87c4e084c5
yesman-noman454 <454jamieowen@gmail.com>
2022-05-17 04:57:22 +0100

    copy from 2 to 3 and 4

--- microbit-4.py
+++ microbit-4.py
@@ -1,6 +1,35 @@
 from microbit import *
 import radio
-radio.config(channel=2)
+radio.on()
+radioChannel=2
+radio.config(channel=radioChannel)
+a=True
+while a:
+    transmit=radio.recive()
+    if transmit!="None" and len(transmit)!=0:
+        a=False
+
+##Needs to do the selection process for what output
+b=randint(0,9)
+radio.send_number(b)
+#a=True
+#while a:
+#    transmitOne=radio.recive()
+#    if transmitOne!="None" and len(transmitOne)!=0:
+#        a=False
+#        if transmitOne == 1:
+#            b=randint(0,9)
+#            radio.send_number(b)
+
+radio.config(channel=10)
+a=True
+while a:
+    transmitTwo=radio.recive()
+    if transmitTwo!="None" and len(transmitTwo)!=0:
+        a=False
+        n=1
+
+
 zero=Image("50505:"
     "05050:"
     "50505:"
@@ -61,13 +90,55 @@ nine=Image("50505:"
     "05550:"
     "50505")
 
+
+wLetter=Image("50005:"
+    "50005:"
+    "50005:"
+    "50505:"
+    "55055")
+
+iLetter=Image("55555:"
+    "00500:"
+    "00500:"
+    "00500:"
+    "55555")
+
+nLetter=Image("50005:"
+    "55005:"
+    "50505:"
+    "50055:"
+    "50005")
+
+bLetter=Image("55550:"
+    "50005:"
+    "55555:"
+    "50005:"
+    "55550")
+gLetter=Image("55555:"
+    "50000:"
+    "50555:"
+    "50005:"
+    "55555")
+
 symbols=[one,two,three,four,five,six,seven,eight,nine]
-def cycle(a):
+
+win=[wLetter,iLetter,gLetter]
+
+big=[bLetter,iLetter,gLetter]
+
+bigwin[big[radioChannel],win[radioChannel]]
+
+def cycle(c,symbols):
     for i in range(5):
         display.show(symbols,delay=100)
-    display.show(symbols[a])
+    display.show(symbols[c])
 
-while True:
-    transmit=radio.recive()
-    if transmit!="None" and len(transmit)!=0:
-        cycle(transmit)
+if n==1:
+    cycle(b,symbols)
+
+control.wait_micros(4000)
+
+if transmitTwo==2:
+    display.show(win[radioChannel])
+elif transmitTwo==3:
+    display.show(bigwin,delay=100)
